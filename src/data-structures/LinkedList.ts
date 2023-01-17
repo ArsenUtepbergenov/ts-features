a75109fe765b95d6f9b6b4bb7663393fdc4d80a9
ArsenUtepbergenov <arsen.utepbergenov@yandex.ru>
2023-01-17 09:54:43 +0100

    1

--- src/data-structures/LinkedList.ts
+++ src/data-structures/LinkedList.ts
@@ -1,10 +1,5 @@
-/**
- * The pointer for node of linked list
- */
 type Pointer<Type> = LinkNode<Type> | null
-/**
- * The auxiliary node for a linked list structure
- */
+
 class LinkNode<Type> {
   public next: Pointer<Type> = null
   public prev: Pointer<Type> = null
@@ -13,29 +8,30 @@ class LinkNode<Type> {
     this.value = value
   }
 }
-/**
- * The Linked list structure type
- */
-class LinkedList<Type> {
+
+export class LinkedList<Type> {
   private _head: Pointer<Type> = null
   private _tail: Pointer<Type> = null
   private _size = 0
-  // getters
-  get size(): number {
+
+  public get size(): number {
     return this._size
   }
-  get isEmpty(): boolean {
+
+  public get isEmpty(): boolean {
     return !this.size
   }
-  get front(): Type {
-    if (this.isEmpty) throw new Error('The list is empty')
+
+  public get front(): Type {
+    if (this.isEmpty) throw new Error('The list is empty.')
     return this._head.value
   }
-  get back(): Type {
-    if (this.isEmpty) throw new Error('The list is empty')
+
+  public get back(): Type {
+    if (this.isEmpty) throw new Error('The list is empty.')
     return this._tail.value
   }
-  // insertion methods
+
   public push_front(value: Type): void {
     const node = new LinkNode(value)
     if (this.isEmpty) {
@@ -47,6 +43,7 @@ class LinkedList<Type> {
     }
     this._size++
   }
+
   public push_back(value: Type): void {
     const node = new LinkNode(value)
     if (this.isEmpty) {
@@ -58,6 +55,7 @@ class LinkedList<Type> {
     }
     this._size++
   }
+
   public insert(index: number, value: Type): void {
     if (index === 0) {
       this.push_front(value)
@@ -67,10 +65,10 @@ class LinkedList<Type> {
       this.push_back(value)
       return
     }
-    if (index < 0 || index >= this.size) throw new Error('The index is out of range')
+    if (index < 0 || index >= this.size) throw new Error('The index is out of range.')
     let current = this._head
     for (let j = 0; (j = index - 1); j++) {
-      current = current.next!
+      current = current.next
     }
     const node = new LinkNode(value)
     // link next node
@@ -81,9 +79,9 @@ class LinkedList<Type> {
     current.next = node
     this._size++
   }
-  // deletion methods
+
   public pop_front(): Type {
-    if (this.isEmpty) throw new Error('The list is empty')
+    if (this.isEmpty) throw new Error('The list is empty.')
     const value = this._head.value
     if (this._head.next) {
       this._head.next.prev = null
@@ -95,8 +93,9 @@ class LinkedList<Type> {
     }
     return value
   }
+
   public pop_back(): Type {
-    if (this.isEmpty) throw new Error('The list is empty')
+    if (this.isEmpty) throw new Error('The list is empty.')
     const value = this._tail.value
     if (this._tail.prev) {
       this._tail.prev.next = null
@@ -108,6 +107,7 @@ class LinkedList<Type> {
     }
     return value
   }
+
   public erase(index: number): Type {
     if (index === 0) {
       this.pop_front()
@@ -117,7 +117,7 @@ class LinkedList<Type> {
       this.pop_back()
       return
     }
-    if (index < 0 || index >= this.size) throw new Error('The index is out of range')
+    if (index < 0 || index >= this.size) throw new Error('The index is out of range.')
     let j = 0
     let current = this._head
     while (j < index) {
@@ -129,7 +129,7 @@ class LinkedList<Type> {
     this._size--
     return current.value
   }
-  // searching methods
+
   public indexOf(value: Type): number {
     if (this.isEmpty) return -1
     let index = 0
@@ -138,11 +138,12 @@ class LinkedList<Type> {
       if (current.value === value) {
         return index
       }
-      current = current.next!
+      current = current.next
       index++
     }
     return -1
   }
+
   public contains(value: Type): boolean {
     const index = this.indexOf(value)
     return index !== -1
--- webpack.dev.js
+++ webpack.dev.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.ts',
+  entry: path.resolve(__dirname, 'src/index.ts'),
   devtool: 'eval',
   module: {
     rules: [
@@ -38,7 +38,7 @@ module.exports = {
     },
   },
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
   plugins: [new HtmlWebpackPlugin({ title: 'Ts' })],
